89c398f28740c539d2e27632749c8ac435b3d423
hoangnam9194 <hoangnam9194@gmail.com>
2025-11-19 15:33:04 +0700

    PolarGX - internal tracking events #16

--- polargx/src/main/java/com/library/polargx/PolarConstants.kt
+++ polargx/src/main/java/com/library/polargx/PolarConstants.kt
@@ -8,8 +8,10 @@ object PolarConstants {
 
     // Delay in milliseconds to retry API request if rate limited (429)
     object API {
-        var DELAY_TO_UPDATE_PROFILE_DURATION_IN_MILLIS = if (Configuration.Env.isDebugging) 0 else 1000L
-        var DELAY_TO_RETRY_API_REQUEST_IF_SERVER_ERROR_IN_MILLIS = if (Configuration.Env.isDebugging) 10000L else 300000L
+        var DELAY_TO_UPDATE_PROFILE_DURATION_IN_MILLIS =
+            if (Configuration.Env.isDebugging) 0 else 1000L
+        var DELAY_TO_RETRY_API_REQUEST_IF_SERVER_ERROR_IN_MILLIS =
+            if (Configuration.Env.isDebugging) 10000L else 300000L
         var DELAY_TO_RETRY_API_REQUEST_IF_TIME_LIMITS_IN_MILLIS = 5000L // 5 seconds
         var DELAY_TO_RETRY_API_REQUEST_IF_CONNECTION_ERROR_IN_MILLIS = 5000L // 5 seconds
     }
@@ -22,12 +24,14 @@ object PolarConstants {
         const val RATE_LIMIT_HTTP_CLIENT = "PolarConstants.RATE_LIMIT_HTTP_CLIENT"
         const val THREAD_LOCKER = "PolarConstants.THREAD_LOCKER"
     }
+
     const val RATE_LIMIT_WAITED_ACTION = "PolarConstants.RATE_LIMIT_WAITED_ACTION"
 
     object DateTime {
         val utcTimeZone = TimeZone.getTimeZone("GMT+00:00")
 
         const val DEFAULT_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'"
+        const val BackendDateTimeMsFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
     }
 
 
@@ -44,6 +48,20 @@ object PolarConstants {
                 const val LINK_DATA_KEY = "LINK_DATA_KEY"
             }
         }
+    }
+
+    object InternalEvent {
+        const val APP_OPEN = "app_open"
+        const val APP_CLOSE = "app_close"
+        const val APP_ACTIVE = "app_active"
+        const val APP_INACTIVE = "app_inactive"
+        const val APP_TERMINATE = "app_terminate"
+
+        const val USER_SESSION_START = "user_session_start"
+
+        const val LINK_CLICK = "link_click"
+
+        const val PUSH_OPEN = "push_open"
     }
 
     object PolarEventKey {
